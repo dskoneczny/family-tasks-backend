31b896a27634b0a13c0386901a31f7d142a1ffeb
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2021-09-30 21:43:01 +0200

    Reorganize errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 
 import { TasksModule } from './tasks/tasks.module';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
 @Module({
   imports: [
     TasksModule,
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message: error.extensions.response.message || error.message,
+        };
+        return graphQLFormattedError;
+      },
     }),
     TypeOrmModule.forRoot({
       type: 'mongodb',
