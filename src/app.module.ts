6042cd43b968fdf8f74cf62b0cfde64b39f1f58c
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2021-09-30 16:50:16 +0200

    Delete obsolete boilerplate files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,12 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
+
 export class AppModule {}
