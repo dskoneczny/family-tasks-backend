22a34d2de79a39d059a477c60d4aae0e3c244312
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2021-10-02 15:58:43 +0200

    Display playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
     GraphQLModule.forRoot({
+      playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       formatError: (error: GraphQLError) => {
         const graphQLFormattedError: GraphQLFormattedError = {
